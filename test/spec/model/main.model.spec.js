0e5a30c2f5f818c64358314a30acd36a6c116894
Albert-Cyberhulk <albstepanyan@gmail.com>
2015-03-03 02:47:09 +0100

    Generator working

--- test/spec/model/main.model.spec.js
+++ test/spec/model/main.model.spec.js
@@ -2,7 +2,7 @@ define(['model/main.model'], function(model) {
 
   describe('A model', function(){
     it('should have certain defaults', function(){
-      expect('Jane'.toBe('Jane'));
+      expect(true).toBe(true);
     });
   });
 
