c38b9a777e8d8862d6ed76ec3cafdfba1c592d67
Cyberhulk <albstepanyan@gmail.com>
2015-07-30 23:53:37 +0200

    Delete templates

--- app/scripts/templates.js
+++
@@ -1,5 +0,0 @@
-this["Templates"] = this["Templates"] || {};
-
-this["Templates"]["main"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<div class=\"jumbotron\">\n  <h1>'Allo, 'Allo!</h1>\n  <p class=\"lead\">\n    <img src=\"images/yeoman.png\" alt=\"I'm Yeoman\"><br>\n    Always a pleasure scaffolding your apps.\n  </p>\n  <p><a class=\"btn btn-lg btn-success\" ng-href=\"#/\">Splendid!<span class=\"glyphicon glyphicon-ok\"></span></a></p>\n</div>\n\n<div class=\"row marketing\">\n  <h4>HTML5 Boilerplate</h4>\n  <p>\n    HTML5 Boilerplate is a professional front-end template for building fast, robust, and adaptable web apps or sites.\n  </p>\n\n  <h4>Angular</h4>\n  <p>\n    AngularJS is a toolset for building the framework most suited to your application development.\n  </p>\n\n  <h4>Karma</h4>\n  <p>Spectacular Test Runner for JavaScript.</p>\n</div>\n dddasdssadsaddsads";
-  },"useData":true});
